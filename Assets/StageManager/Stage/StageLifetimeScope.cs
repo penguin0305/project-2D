d0976eb89888916c706307ba8bbb2ef8cd6e863e
seiad00 <tlsdnjsdud51@naver.com>
2026-02-07 23:13:10 +0900

    스테이지 진행 문제 수정

--- Assets/StageManager/Stage/StageLifetimeScope.cs
+++ Assets/StageManager/Stage/StageLifetimeScope.cs
@@ -9,11 +9,17 @@ public class StageLifetimeScope : LifetimeScope
     [SerializeField] private StageManager StageManager;
     [SerializeField] private SceneSetup SceneSetup;
     [SerializeField] private MapLoader MapLoader;
+    //[SerializeField] private EndPortal EndPortal;
+    [SerializeField] private Boss Boss;
+    
 
     protected override void Configure(IContainerBuilder builder)
     {
         base.Configure(builder);
 
+        //builder.RegisterComponentInHierarchy<EndPortal>().AsImplementedInterfaces();
+
+        builder.RegisterComponent(Boss);
         builder.RegisterComponent(MapLoader);
         builder.RegisterComponent(CameraFollow);
         builder.RegisterComponent(StageManager);
