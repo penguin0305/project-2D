7c6537697d5bec0c634cd37bdc3909386e4ed88c
seiad00 <tlsdnjsdud51@naver.com>
2026-05-03 19:00:38 +0900

    tq

--- Assets/StageManager/Stage/StageLifetimeScope.cs
+++ Assets/StageManager/Stage/StageLifetimeScope.cs
@@ -10,7 +10,7 @@ public class StageLifetimeScope : LifetimeScope
     [SerializeField] private SceneSetup SceneSetup;
     [SerializeField] private MapLoader MapLoader;
     //[SerializeField] private EndPortal EndPortal;
-    [SerializeField] private Boss Boss;
+    //[SerializeField] private Boss Boss;
     [SerializeField] private CameraFollow CameraFollow;
 
 
@@ -20,7 +20,7 @@ public class StageLifetimeScope : LifetimeScope
 
         //builder.RegisterComponentInHierarchy<EndPortal>().AsImplementedInterfaces();
 
-        builder.RegisterComponent(Boss);
+        //builder.RegisterComponent(Boss);
         builder.RegisterComponent(MapLoader);
         builder.RegisterComponent(CameraFollow);
         builder.RegisterComponent(StageManager);
